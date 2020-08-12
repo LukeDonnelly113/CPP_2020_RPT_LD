39b5219db39c10e6f9ab748d4da681f22cf7af62
D00201576 <47150133+D00201576@users.noreply.github.com>
2020-08-12 22:31:10 +0100

    Working on adding the overloaded operators to the user.cpp file.

---
+++ CPP_2020_RPT_LD/user.cpp
@@ -0,0 +1,89 @@
+#include "user.h"
+
+user::user()
+{
+	
+}
+user::user(string email, string password, string userName, time_t lastLogin)
+{
+	setemail(email);
+	setpassword(password);
+	setuserName(userName);
+	setlastLogin(lastLogin);
+}
+
+string user::getemail()const
+{
+	return email;
+}
+void user::setemail(string email)
+{
+	if (email.length() >= 6)
+	{
+		this->email = email;
+	}
+}
+
+string user::getpassword()const
+{
+	return password;
+}
+void user::setpassword(string password)
+{
+	if (password.length() >= 6)
+	{
+		this->password = password;
+	}
+}
+
+string user::getuserName()const
+{
+	return userName;
+}
+void user::setuserName(string userName)
+{
+	if (userName.length() >= 6)
+	{
+		this->userName = userName;
+	}
+}
+
+time_t user::getlastLogin()const
+{
+	return lastLogin;
+}
+void user::setlastLogin(time_t lastLogin)
+{
+	if (lastLogin > 00.00)
+	{
+		this->lastLogin = lastLogin;
+	}
+}
+
+//overloaded operators
+ostream& operator<<(ostream& os, const user User)
+{
+	//os << user.email << ", " << user.password << ", " << user.userName << ", " << user.lastLogin;
+	return os;
+
+}
+istream& operator>>(istream& is, user User)
+{
+	//is >> user.email >> user.password >> user.userName >> user.lastLogin;
+	return is;
+}
+user& user::operator=(const user& User)
+{
+	if (this == &User)
+	{
+		return *this;
+	}
+	else
+	{
+		email = User.getemail();
+		password = User.getpassword();
+		userName = User.getuserName();
+		lastLogin = User.getlastLogin();
+		return *this;
+	}
+}
